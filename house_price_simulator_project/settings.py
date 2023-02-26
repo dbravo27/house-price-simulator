7b11d6fec9af73660bff13a981eb7b8ddc51f114
Diego Bravo <dbravo27@gmail.com>
2023-02-26 13:57:15 -0300

    add house model

--- house_price_simulator_project/settings.py
+++ house_price_simulator_project/settings.py
@@ -80,13 +80,12 @@ WSGI_APPLICATION = "house_price_simulator_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": 'django.db.backends.postgresql',
-        "NAME": os.environ.get('DB_NAME'),
-	"USER": os.environ.get('DB_USER'),
-	"PASSWORD": os.environ.get('DB_PASSWORD'),
-	"HOST": 'localhost',
-	"PORT": '',
-
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
